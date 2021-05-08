4142a2a1461de027cbef9e140a902bde682da4f7
VK248 <76945809+VK248@users.noreply.github.com>
2021-05-08 15:43:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+name_of_the_student_array=[];
+
+
+
+function submit()
+{
+var name1=document.getElementById("nameofstudent1").value;
+var name2=document.getElementById("nameofstudent2").value;
+var name3=document.getElementById("nameofstudent3").value;
+var name4=document.getElementById("nameofstudent4").value;
+
+name_of_the_student_array.push(name1);
+name_of_the_student_array.push(name2);
+name_of_the_student_array.push(name3);
+name_of_the_student_array.push(name4);
+
+console.log=(name_of_the_student_array);
+
+document.getElementById("display_name").innerHTML=name_of_the_student_array;
+document.getElementById("bton").style.display="none";
+document.getElementById("bton2").style.display="inline-block";
+}
+function sorting()
+{
+    name_of_the_student_array.sort();
+    console.log=(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML=name_of_the_student_array;
+}
